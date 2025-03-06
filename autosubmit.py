9f7b2acde35df250ac5cb655fdbc77b9e28a8646
BrattyLittleBunny <robinflight223@gmail.com>
2025-03-05 19:32:50 -0600

    Update autosubmit.py
    Got the Selenium to successfully submit!!!
    Stole some code from ArachnidInner to figure out what i did wrong.
    added proxie support to the driver.

--- autosubmit.py
+++ autosubmit.py
@@ -1,16 +1,18 @@
-# [X] made regular http requests
-# [~] Checks to see if selenium is needed
-#	-Doesn't really check, more just assumes if requests doesn't work.
-# [~] Runs in selenium with all the information already gathered
-#	[~] Runs - kinda? it will do it, just easily detected...
-#	[ ] Driver runs in background
-# [~] Runs a through a proxy if desired.
-#	[X] Requests Proxy
-#	[ ] Webdriver Proxy
-# [ ] Dynamically (big word :O) checks and changes the headers for the GET/POST
-# [ ] check for recaptcha and csrf tokens.
-# [X] Get the form daya to be accessible for selenium as well :3
-
+'''
+[X] made regular http requests
+[~] Checks to see if selenium is needed
+	-Doesn't really check, more just assumes if requests doesn't work.
+[X] Runs in selenium with all the information already gathered
+	[X] Runs
+	[F] Driver runs in background - doesn't work D:
+[X] Runs a through a proxy if desired.
+	[X] Requests Proxy
+	[X] Webdriver Proxy
+[ ] Dynamically (big word :O) checks and changes the headers for the GET/POST
+[~] check for recaptcha and csrf tokens.
+	[X] Checks for csrf token
+	[ ] Checks for recaptcha
+'''
 import requests
 import sys
 import time
@@ -20,11 +22,25 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 #global declarations
 fake = Faker()
 form_data = {}
-url_index = 1#using this to check if we need to do another form data grab
+
+#slowly types to make it seem more human
+def slow_type(element, text):
+	"""
+	Type text slowly with a small delay between characters
+	Adds realism and visibility to typing process
+	stolen from arachnidInner. is a good idea :3
+	"""
+	for char in text:
+		element.send_keys(char)
+		time.sleep(random.uniform(0.09, 0.19))
 
 
 def _cookies_grabber():
@@ -35,7 +51,8 @@ def _headers_grabber():
 	print("to be filled in")
 
 #to grab the page info for requests
-def _form_grab(input_form):
+#original plan was to have this for both the driver and requests, but can't figure out how to do it XP
+def _requests_form_grab(input_form):
 	#check for the user input fields
 	#setting up the submission data :D
 	global form_data	
@@ -84,7 +101,7 @@ def _form_grab(input_form):
 				
 	
 #web driver
-def _driver_session(driver,link, proxies):
+def _driver_session(driver,link):
 	
 	driver.get(link)
 	driver.implicitly_wait(2)
@@ -104,26 +121,35 @@ def _driver_session(driver,link, proxies):
 			continue
 		
 		if field.tag_name == "textarea":  
-			field.send_keys(fake.paragraph(nb_sentences=random.randint(3, 10)))
+			print("Typing random text for big area")
+			slow_type(field, fake.paragraph(nb_sentences=random.randint(3, 10)))
 		
 		elif fieldType == "text":# and fieldName:
 			if "first_name" in fieldName or "first-name" in fieldName:
-				field.send_keys(fake.first_name())
+				print("Typing first name")
+				slow_type(field, fake.first_name())
 			elif "last_name" in fieldName or "last-name" in fieldName:
-				field.send_keys(fake.last_name())
+				print("Typing last name")
+				slow_type(field, fake.last_name())
 			elif "name" in fieldName or "full-name" in fieldName or "full_name" in fieldName:
-				field.send_keys(fake.name())
+				print("Typing full name")
+				slow_type(field, fake.name())
 			elif "location" in fieldName:
-				field.send_keys(fake.address())
+				print("Typing locatioin")
+				slow_type(field, fake.address())
 			elif "zipcode" in fieldName:
-				field.send_keys(str(random.randint(10000, 99999)))
+				print("Typing ZipCode")
+				slow_type(field, str(random.randint(10000, 99999)))
 			else:
-				field.send_keys(fake.paragraph(nb_sentences=random.randint(3,10)))
+				print("Typing random text")
+				slow_type(field, fake.paragraph(nb_sentences=random.randint(3,10)))
 		elif fieldType == "email":
-			field.send_keys(fake.email())
+			print("Typing email")
+			slow_type(field, fake.email())
 
 		elif fieldType == "number":
-			field.send_keys(fake.phone_number())
+			print("Typing phone number")
+			slow_type(field, fake.phone_number())
 
 		elif fieldType == "radio":
 			# Group radio buttons by name
@@ -135,19 +161,23 @@ def _driver_session(driver,link, proxies):
 		elif fieldType == "checkbox":
 			# Randomly check/uncheck the checkbox
 			if random.choice([True, False]):
+				print("Clicking Check Box")
 				field.click()
 				
 		elif fieldTag == 'textarea':
-			form_data[field_Name] = fake.paragraph(nb_sentences=50)
+			print("Typing LARGE paragraph name")
+			slow_type(field, fake.paragraph(nb_sentences=50))
 
 	# Randomly selecting and clicking one radio button from each group
 	for radio_group in radio_groups.values():
+		print("Clicking radio group")
 		random.choice(radio_group).click()
 
 	try:
 		submit_button = driver.find_element(By.XPATH, "//button[@type='submit' or contains(text(), 'Submit') or contains(text(), 'Sign Up')]")
 		driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", submit_button)
 		time.sleep(1.5)
+		print("Clicking submit")
 		submit_button.click()
 	except Exception as e:
 		print(f"Submit button not found: {e}")
@@ -169,7 +199,7 @@ def _driver_session(driver,link, proxies):
 			continue
 
 	# Alternative check: If URL changes, assume success
-	if "success" in driver.current_url or "thank-you" in driver.current_url:
+	if "success" in driver.current_url or "thank-you" in driver.current_url or "thankyou" in driver.current_url:
 		print("Driver success")
 		return True
 	
@@ -223,7 +253,7 @@ def _request_session(link, proxies):
 		submit_URL = link #+ submit_URL
 		#print(submit_URL)
 	
-	_form_grab(input_form)
+	_requests_form_grab(input_form)
 	
 	
 	if proxies:
@@ -270,7 +300,8 @@ def _request_session(link, proxies):
 
 #main loop
 def main(): 
-	url = [<Put URLS HERE IN THIS LIST =>:3>]
+	url = ["<put list of URLs you want"]
+	#proxies for if you want them
 	#proxieThings = {"http" : "socks5h://127.0.0.1:9150", "https" : "socks5h://127.0.0.1:9150"}
 	proxieThings = False
 	code_run = True
@@ -278,37 +309,62 @@ def main():
 	for x in url:
 		#request attempts
 		requests_run = True
+		counter = 1
 		while requests_run:
+			print(f"Requests submission #{counter} starting")
 			requests_run = _request_session(x, proxieThings)
 			if requests_run:
 				waitTime = (str(random.randint(0,5))+"."+str(random.randint(0,9)))
-				print("Requests Waiting " + waitTime + " seconds")
+				print(f"Request submmission #{counter} for {x} is successful")
+				counter += 1
+				print(f"Requests Waiting {waitTime} seconds")
 				time.sleep(float(waitTime))
 				#continue
 			else:
 				print(f"Requests stop for {x}")
+				print(f"Submitted {counter} times.")
 		
 		#driver attempts
 		driver_run = True
 		#putting webdriver here so it'll be a persistant session.
+		print("Setting up Driver options!")
 		firefox_options = Options()
-		firefox_options.headless = True  # Optional: run without opening the browser
-		firefox_options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-
+		firefox_options.add_argument("-profile") 
+		firefox_options.add_argument("<Input non cache firefox profile here>")
+		#firefox_options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+		#if you have proxies you wanna use, this will activate. just put them in the proxieThings part :3
+		if proxieThings:
+			print("setting up driver proxy :D")
+			proxy = Proxy()
+			proxy.proxy_type = ProxyType.MANUAL
+			try:
+				proxy.http_proxy = proxieThings["http"]  # Example: "192.168.1.100:8080"
+			except:
+				print("no http proxy found D:")
+			try:
+				proxy.ssl_proxy = proxieThings["ssl"]  # Example: "192.168.1.100:8080"
+			except:
+				print("no ssl proxy found D:")
+			firefox_options.set_proxy(proxy)
 		driver = webdriver.Firefox(options=firefox_options)
+		print("Driver ready")
+		counter = 1
 		while driver_run:
-			driver_run = _driver_session(driver,x, proxieThings)
+			print(f"Driver submission #{counter} starting")
+			driver_run = _driver_session(driver,x)
 			time.sleep(0.5)
 			if driver_run:
-				waitTime = (str(random.randint(0,5))+"."+str(random.randint(0,9)))
+				print(f"Driver submission #{counter} for {x} was successful")
+				counter +=1
+				waitTime = (str(random.randint(0,9))+"."+str(random.randint(0,9)))
 				print(f"Driver Waiting {waitTime} seconds")
 				time.sleep(float(waitTime))
 			else:
 				print(f"Driver stop for {x}")
+				print(f"submitted {counter} times")
 				driver.quit()
 				
 		print(f"End of submissions for {x}")
 		
 if __name__ == "__main__":
 	main()
-
